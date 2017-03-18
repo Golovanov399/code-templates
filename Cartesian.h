d45b12e04aa4d7d83f572c0cb556c2181201f35a
Golovanov399 <Golovanov12345@gmail.com>
2017-03-18 14:17:44 +0300

    edit Cartesian tree so that it now doesn't make one's eyes bleed

--- Cartesian.h
+++ Cartesian.h
@@ -1,107 +1,164 @@
-struct Node {
-	int value;
-	int prior;
-	int sz;
-	char rev;
-	Node *l, *r;
+template <typename T>
+struct Cartesian {
+	struct Node {
+		T data;
+		int prior;
+		int size;
+		Node *left, *right;
+		bool rev;
 
-	Node(int value):value(value){
-		sz = 1;
-		prior = rand();
-		rev = 0;
-	}
+		explicit Node(const T& val) {
+			data = val;
+			prior = rand();
+			left = right = nullptr;
+			size = 1;
+			rev = false;
+		}
 
-	void relax(){
-		sz = 1;
-		if (l)
-			sz += l->sz;
-		if (r)
-			sz += r->sz;
-	}
+		~Node() {
+			if (left) {
+				delete left;
+			}
+			if (right) {
+				delete right;
+			}
+		}
+	};
 
-	void relrev(){
-		if (rev){
-			swap(l, r);
-			if (l)
-				l->rev ^= 1;
-			if (r)
-				r->rev ^= 1;
-			rev = 0;
+	void relax(Node* node) {
+		node->size = 1;
+		// update your statistics here
+		if (node->left) {
+			node->size += node->left->size;
+		}
+		if (node->right) {
+			node->size += node->right->size;
 		}
 	}
 
-	pair<Node*, Node*> split(int x){
-		if (x == 0)
-			return {NULL, this};
-		else if (x == sz)
-			return {this, NULL};
-		else {
-			relrev();
-			int lsz = l ? l->sz : 0;
-			if (x <= lsz){
-				auto p = l->split(x);
-				l = p.second;
-				relax();
-				return {p.first, this};
-			} else
-			if (x == lsz + 1){
-				Node* tmp = r;
-				r = NULL;
-				relax();
-				return {this, tmp};
+	void relax_rev(Node* node) {
+		if (node->rev) {
+			swap(node->left, node->right);
+			if (node->left) {
+				node->left->rev ^= 1;
+			}
+			if (node->right) {
+				node->right->rev ^= 1;
+			}
+			node->rev = 0;
+		}
+	}
+
+	pair<Node*, Node*> split(Node* node, const T& val) { // (<=, >)
+		if (!node) {
+			return {nullptr, nullptr};
+		}
+		// no reverse!
+		if (node->data <= val) {
+			auto tmp = split(node->right, val);
+			node->right = tmp.first;
+			relax(node);
+			return {node, tmp.second};
+		} else {
+			auto tmp = split(node->left, val);
+			node->left = tmp.second;
+			relax(node);
+			return {tmp.first, node};
+		}
+	}
+
+	pair<Node*, Node*> split_by_cnt(Node* node, int cnt) { // (cnt, n - cnt)
+		if (!node) {
+			assert(cnt == 0);
+			return {nullptr, nullptr};
+		}
+		relax_rev(node);
+		int lsz = (node->left ? node->left->size : 0);
+		if (cnt > lsz) {
+			auto tmp = split_by_cnt(node->right, cnt - lsz - 1);
+			node->right = tmp.first;
+			relax(node);
+			return {node, tmp.second};
+		} else {
+			auto tmp = split_by_cnt(node->left, cnt);
+			node->left = tmp.second;
+			relax(node);
+			return {tmp.first, node};
+		}
+	}
+
+	Node* merge(Node* first, Node* second) {
+		if (!first) {
+			return second;
+		} else if (!second) {
+			return first;
+		} else {
+			if (first->prior < second->prior) {
+				relax_rev(first);
+				first->right = merge(first->right, second);
+				relax(first);
+				return first;
 			} else {
-				x -= lsz + 1;
-				auto p = r->split(x);
-				r = p.first;
-				relax();
-				return {this, p.second};
+				relax_rev(second);
+				second->left = merge(first, second->left);
+				relax(second);
+				return second;
 			}
 		}
 	}
-};
 
-Node* merge(Node *l, Node *r){
-	if (!l)
-		return r;
-	if (!r)
-		return l;
-	if (l->prior > r->prior){
-		l->relrev();
-		l->r = merge(l->r, r);
-		l->relax();
-		return l;
-	} else {
-		r->relrev();
-		r->l = merge(l, r->l);
-		r->relax();
-		return r;
+	template <typename Callback>
+	void in_order(Node* node, Callback callback) {
+		if (!node) {
+			return;
+		}
+		relax_rev(node);
+		in_order(node->left, callback);
+		callback(node->data);
+		in_order(node->right, callback);
 	}
-}
 
-struct Cart{
+	// Cartesian interface starts here
+
 	Node *root;
 
-	Cart(){
-		root = new Node(0);
+	Cartesian() {
+		root = nullptr;
 	}
 
-	void add(int x){
-		Node *tmp = new Node(x);
-		root = merge(root, tmp);
+	~Cartesian() {
+		delete root;
 	}
 
-	void rev(int l, int r){
-		auto p = root->split(l - 1);
-		auto q = p.second->split(r - l + 1);
-		q.first->rev ^= 1;
-		root = merge(p.first, merge(q.first, q.second));
+	void append(const T& val) {
+		auto new_node = new Node(val);
+		root = merge(root, new_node);
 	}
 
-	int pop(int v){
-		auto p = root->split(v);
-		auto q = p.second->split(1);
-		int res = q.first->value;
-		root = merge(p.first, q.second);
-		return res;
+	void push_back(const T& val) {
+		append(val);
+	}
+
+	void add(const T& val, int pos) {
+		auto p = split_by_cnt(root, pos);
+		root = merge(p.first, merge(new Node(val), p.second));
+	}
+
+	void reverse(int l, int r) { // [l, r)
+		if (r <= l) {
+			return;
+		}
+		auto p = split_by_cnt(root, r);
+		auto q = split_by_cnt(p.first, l);
+		q.second->rev ^= 1;
+		root = merge(merge(q.first, q.second), p.second);
+	}
+
+	vector<T> get_all() {
+		vector<T> result;
+		in_order(root, [&result](const T& data){
+			result.push_back(data);
+		});
+		return result;
 	}
 };
